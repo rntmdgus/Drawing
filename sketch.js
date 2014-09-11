4d0cb17941b82d6db1ddcaf8d9f390c8081d5b30
rntmdgus <rntmdgus@naver.com>
2014-09-11 13:37:44 +0900

    1
    1

--- sketch.js
+++ sketch.js
@@ -1,6 +1,9 @@
 function setup() {
   createCanvas(640, 480);
-  background(0);
+
+  // 0:black, 255: white
+  // background(red, green, blue) R, G, B
+  background(151, 214, 196);
   stroke(255);
   noFill();
   line(10, 10, 90, 10);
